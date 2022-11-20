78638293b533a751379eba9b5eddda3da144c0cf
FelipeDonatto <fe.donatto@outlook.com>
2022-11-20 16:03:37 -0300

    requisito 02 finalizado

---
+++ src/Races/Orc.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Orc extends Race {
+  private _maxLifePoints: number;
+  private static _counter = 0;
+
+  constructor(_name: string, _dexterity: number) {
+    super(_name, _dexterity);
+    this._maxLifePoints = 74;
+    Orc._counter += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return this._counter;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Orc;
