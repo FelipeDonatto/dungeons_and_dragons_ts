353dc0b9f7a5d3b0a748aa4632316cffad430fd0
FelipeDonatto <fe.donatto@outlook.com>
2022-11-21 15:13:52 -0300

    requisito 6 finalizado

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,17 @@
+import Energy from '../Energy';
+
+interface FighterStats {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: Energy;
+}
+
+interface Fighter extends FighterStats {
+  attack(enemy: FighterStats): void;
+  special?(enemy: FighterStats): void;
+  levelUp(): void;
+  receiveDamage(attackPoints: number): number;
+}
+
+export default Fighter;
